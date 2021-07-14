2db72be74f3f3aec60c11d14ca2f02b9c163dad8
Kohei Akiyama <kohei.akiyama@jbs.com>
2021-07-14 19:02:27 +0900

    Facade途中まで

---
+++ Chapter15_Facade/Pagemaker/Database.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+
+namespace Chapter15_Facade.Pagemaker
+{
+    public class Database
+    {
+        private Database()
+        {
+            
+        }
+
+        public static Dictionary<string, string> GetProperties(string dbname)
+        {
+            string filename = dbname + ".txt";
+            var result = new Dictionary<string, string>();
+
+            using (var file = new StreamReader("./" + filename))
+            {
+                string line;
+
+                while ((line = file.ReadLine()) != null)
+                {
+                    var arrayLine = line.Split("=").ToArray();
+                    result.Add(arrayLine[0], arrayLine[1]);
+                }
+            }
+
+            return result;
+        }
+    }
+}
---
+++ Chapter15_Facade/Pagemaker/HtmlWriter.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+namespace Chapter15_Facade.Pagemaker
+{
+    public class HtmlWriter
+    {
+        private StreamWriter _sw;
+
+        public HtmlWriter(StreamWriter streamWriter)
+        {
+            _sw = streamWriter;
+        }
+
+        public void Title(string title)
+        {
+
+        }
+    }
+}
