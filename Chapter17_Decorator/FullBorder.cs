9254c67547ec77a7cb2caf4b5b941ee38c0d10ec
achiyama73 <akiyama9573@gmail.com>
2022-01-25 01:16:48 +0900

    Chapter12 Decoratorパターンを追加

---
+++ Chapter17_Decorator/FullBorder.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Chapter17_Decorator
+{
+    /// <summary>
+    /// 上下左右に飾り枠をつけるクラス
+    /// </summary>
+    public class FullBorder : Border
+    {
+        public FullBorder(Display display) : base(display)
+        {
+        }
+
+        public override int GetColumns()
+        {
+            return 1 + _display.GetColumns() + 1;
+        }
+
+        public override int GetRows()
+        {
+            return 1 + _display.GetRows() + 1;
+        }
+
+        public override string GetRowText(int row)
+        {
+            if (row == 0)
+            {
+                // 枠の上端
+                return "+" + MakeLine('-', _display.GetColumns()) + "+";
+            }
+            else if (row == _display.GetRows() + 1)
+            {
+                // 枠の下端
+                return "+" + MakeLine('-', _display.GetColumns()) + "+";
+            }
+            else
+            {
+                // それ以外
+                return "|" + _display.GetRowText(row - 1) + "|";
+            }
+        }
+
+        /// <summary>
+        /// 文字chをcount個連続させた文字列を作る
+        /// </summary>
+        /// <param name="ch"></param>
+        /// <param name="count"></param>
+        /// <returns></returns>
+        private string MakeLine(char ch, int count)
+        {
+            var sb = new StringBuilder();
+            
+            for (var i = 0; i < count; i++)
+            {
+                sb.Append(ch);
+            }
+
+            return sb.ToString();
+        }
+    }
+}
---
+++ Chapter17_Decorator/StringDisplay.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Chapter17_Decorator
+{
+    /// <summary>
+    /// 1行だけからなる文字列表示用のクラス
+    /// </summary>
+    public class StringDisplay : Display
+    {
+        /// <summary>
+        /// 表示文字列
+        /// </summary>
+        private string _string;
+
+        public StringDisplay(string str)
+        {
+            _string = str;
+        }
+
+        public override int GetColumns()
+        {
+            Encoding.RegisterProvider(CodePagesEncodingProvider.Instance);
+            Encoding sjisEnc = Encoding.GetEncoding("Shift_JIS");
+            var length = sjisEnc.GetByteCount(_string);
+            return length;
+        }
+
+        public override int GetRows()
+        {
+            return 1;
+        }
+
+        public override string GetRowText(int row)
+        {
+            if (row == 0)
+            {
+                return _string;
+            }
+
+            return null;
+        }
+    }
+}
