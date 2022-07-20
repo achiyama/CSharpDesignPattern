bd6e8ea02d6fcd7b5637722e8ef8e7f7dc6582d5
achiyama <achiy4ma@gmail.com>
2022-07-20 17:29:18 +0900

    Abstract Factoryの追加(途中)

---
+++ Chapter08_AbrstractFactory/Factory/Item.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Chapter08_AbrstractFactory.Factory;
+
+public abstract class Item
+{
+    protected string _caption;
+
+    public Item(string caption)
+    {
+        _caption = caption;
+    }
+
+    public abstract string MakeHtml();
+}
