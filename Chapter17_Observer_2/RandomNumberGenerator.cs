c8ba615da4c1ae44b4ccd68b34f7261c1cefc2a8
Kohei Akiyama <kohei.akiyama@outlook.jp>
2022-07-31 22:50:54 +0900

    OberverをRxで実現しようと模索中・・・

---
+++ Chapter17_Observer_2/RandomNumberGenerator.cs
@@ -0,0 +1,16 @@
+﻿namespace Chapter17_Observer_2;
+
+public class RandomNumberGenerator : IObservable<NumberGenerator>
+{
+    private IList<IObservable<NumberGenerator>> _observers;
+
+    public RandomNumberGenerator()
+    {
+        _observers = new List<IObservable<NumberGenerator>>();
+    }
+
+    public IDisposable Subscribe(IObserver<NumberGenerator> observer)
+    {
+        throw new NotImplementedException();
+    }
+}
